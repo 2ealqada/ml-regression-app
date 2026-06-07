21748e17d0fa0572acdb44c6614ac2794411ca50
2ealqada <eng.iqbal201893@gmail.com>
2026-06-07 21:47:26 +0200

    Add initial Streamlit app

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st 
+st.set_page_config(page_title = "ML Regression App", page_icon = "🤖",layout= "wide")
+st.title("ML Regression App")
+st.subheader("Random Forest vs. kNN")
+st.write("Diese App vergleicht zwei Machine-Learning-Modelle "
+    "für eine Regressionsaufgabe.")
+st.info("Projekt basiert auf dem praktischen Teil meiner Bachelorarbeit.")
